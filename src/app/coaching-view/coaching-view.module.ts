9d38be0aa8cffacc2e30049034798b2ea1295e00
lgaskil <lgaskill91@gmail.com>
2018-10-30 20:32:19 -0500

    Added more scroll animations. App title now links to the home page.

--- src/app/coaching-view/coaching-view.module.ts
+++ src/app/coaching-view/coaching-view.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CoachingViewComponent } from "./coaching-view.component";
 import { SharedModule } from "../shared/shared.module";
 import { AppBarModule } from "../shared/app-bar/app-bar.module";
+import { ScrollAnimationModule } from "../shared/scroll-animation/scroll-animation.module";
 
 @NgModule({
-  imports: [SharedModule, AppBarModule],
+  imports: [SharedModule, AppBarModule, ScrollAnimationModule],
   declarations: [CoachingViewComponent],
   providers: []
 })
