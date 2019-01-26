3d73651eb197272130bf623ddd5c49205c6ef839
lgaskil <lgaskill91@gmail.com>
2019-01-26 16:33:50 -0600

    feat: SSL config files, re-enabling CRUD routes

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
   public static API_HOST_LOCAL = "http://localhost:8081";
-  public static API_HOST_PROD = "http://www.theglowblueprint.com";
+  public static API_HOST_PROD = "https://www.theglowblueprint.com";
 
   public static API_KEY = "dcb76dac-81da-4f78-9a22-e458b33114d0";
 }
