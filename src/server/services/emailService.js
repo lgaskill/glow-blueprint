8e5a5570df027540b207e2575caa3be0014e74d3
lgaskil <lgaskill91@gmail.com>
2019-08-14 22:22:32 -0500

    feat: Email Notifications

--- src/server/services/emailService.js
+++ src/server/services/emailService.js
@@ -1,115 +1,58 @@
-const fs = require("fs");
-const readline = require("readline");
+const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
-// TODO: REMOVE!!!!!!!!!!!!!!!!!!!!!!!
-const temp = {
-  installed: {
-    client_id:
-      "425604795395-5n7gmmv5c6ppkpbk8rtjl71ncoo4fr7l.apps.googleusercontent.com",
-    project_id: "theglowblueprint-1552441299073",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_secret: "JJm9GJoZ8eUpY-p60iV7g6UN",
-    redirect_uris: ["urn:ietf:wg:oauth:2.0:oob", "http://localhost:4200"]
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN;
+
+const oauth2Client = new OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+exports.send = async ({ subject, message, recipients }) => {
+  let accessToken;
+  try {
+    accessToken = await oauth2Client.getAccessToken();
+  } catch (err) {
+    console.error("Failed to get access token", err);
+    return;
   }
-};
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
-
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = "token.json";
-
-// // Load client secrets from a local file.
-// fs.readFile('credentials.json', (err, content) => {
-//   if (err) return console.log('Error loading client secret file:', err);
-//   // Authorize a client with credentials, then call the Gmail API.
-//   authorize(JSON.parse(content), listLabels);
-// });
-authorize(temp, listLabels);
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-  // const authUrl = oAuth2Client.generateAuthUrl({
-  //   access_type: "offline",
-  //   scope: SCOPES
-  // });
-  // console.log("Authorize this app by visiting this url:", authUrl);
-  // const rl = readline.createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout
-  // });
-  // rl.question("Enter the code from that page here: ", code => {
-  //   rl.close();
-  oAuth2Client.getToken(
-    "4/DAEWCPL316WuaLDrC7h0P8d-zjf_reUY8mJvCruEloNoU9swl0CysiQ",
-    (err, token) => {
-      if (err) return console.error("Error retrieving access token", err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-        if (err) return console.error(err);
-        console.log("Token stored to", TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    }
-  );
-  // });
-}
-
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listLabels(auth) {
-  const gmail = google.gmail({ version: "v1", auth });
-  gmail.users.labels.list(
-    {
-      userId: "abby@theglowblueprint.com"
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: "abby@theglowblueprint.com",
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESH_TOKEN,
+      accessToken
     },
-    (err, res) => {
-      if (err) return console.log("The API returned an error: " + err);
-      const labels = res.data.labels;
-      if (labels.length) {
-        console.log("Labels:");
-        labels.forEach(label => {
-          console.log(`- ${label.name}`);
-        });
+    tls: { rejectUnauthorized: false }
+  });
+
+  const mailOptions = {
+    from: "abby@theglowblueprint.com",
+    to: recipients.join(", "),
+    subject: subject,
+    generateTextFromHTML: true,
+    html: message
+  };
+
+  return new Promise((resolve, reject) => {
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+      smtpTransport.close();
+      if (!error) {
+        console.error(error);
+        reject(error);
       } else {
-        console.log("No labels found.");
+        console.log("Email Sent");
+        resolve(response);
       }
-    }
-  );
-}
+    });
+  });
+};
